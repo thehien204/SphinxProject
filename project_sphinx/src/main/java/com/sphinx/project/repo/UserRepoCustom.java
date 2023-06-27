225b4a50acbd632ce0aa680123963a1ba46344eb
hien <phamthehienc20042000@gmail.com>
2023-06-27 14:06:13 +0700

    a

--- project_sphinx/src/main/java/com/sphinx/project/repo/UserRepoCustom.java
+++
@@ -1,7 +0,0 @@
-package com.sphinx.project.repo;
-
-
-public interface UserRepoCustom {
-//    ListCount findUserByConditation (UserDTO userDTO, Pageable pageable);
-//    ListCount findAllUserByStatus (UserDTO userDTO, Pageable pageable);
-}
